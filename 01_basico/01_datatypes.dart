72a1a1c76e2df5f87bad246d3cef2a781bba617d
John Contreras <john.contreras@aveco.com>
2022-03-12 17:41:04 -0500

    Typos de datos

--- 01_basico/01_datatypes.dart
+++
@@ -1,5 +0,0 @@
-main(){
-
-  print("Hola Mundo");
-
-}
---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,15 @@
+main(){
+
+  // === numeros var acepta todos los tipos de variables
+  int a = 10;
+  double b = 5.5;
+  // ? significa que puede ser nulo
+  int? c = null;
+
+  int a_ = 30;
+  double $b = 60;
+  print(a_ + $b);
+
+ 
+  print(c);
+}
